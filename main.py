5b16a7dc1f361116a1487a97b016e997b836c3b3
wakawakathedev <wakawakathedev@gmail.com>
2021-12-05 16:16:11 +0800

    WIP

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+import requests
 
 from flask import Flask, request
-from flask_discord_interactions import DiscordInteractions
+from flask_discord_interactions import DiscordInteractions, Message, Embed, embed
 
 app = Flask(__name__)
 discord = DiscordInteractions(app)
@@ -20,11 +21,36 @@ def test(ctx):
     "Respond with I am not a test!"
     return "I am an updated a test!"
 
+# @discord.command()
+# def check_balance(ctx):
+#     "This should get the balance from the pv"
+
+#     ctx.accon
+#     r = requests.get("http://52.52.160.149/accounts/0067a4d2b153f62041a9cca5454aebd06ea1d0827828da889ddaad991d077401/balance?format=json")
+#     print(r.json())
+#     return "Check balance"
+
+@discord.command()
+def help(ctx):
+    "Help Message for using the bot"
+
+    return Message(embed = Embed(
+        title = "Help",
+        description = "Help description",
+        fields=[
+            embed.Field(
+                name= "Command: `/check_balance`",
+                value= "Responds with Pong!"
+            )
+        ]
+    ))
+
 @app.route('/', methods=["GET"])
 def hello_world():
     return "Hello World"
 
 discord.set_route("/api/interactions")
+discord.update_commands()
 
 if __name__ == "__main__":
     app.run(debug=True)
